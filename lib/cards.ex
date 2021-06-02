6dbc5d3ff039c8e277fad5615bf29e92579162ce
Hayden <hpiper3097@gmail.com>
2021-06-02 00:43:19 -0500

    added shuffle, contains?;  solved nested list problem

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,24 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three", "Four", "Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      value <> " of " <> suit
+    end
+  end
+
+  #my function to solve the nested lists problem
+  #also see List.flatten($listname)
+  def init do
+    create_deck() |> Enum.concat()
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
