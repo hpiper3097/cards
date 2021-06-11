60a31ad4b05180efe48bad5abd451fed9739ceb7
Hayden <hpiper3097@gmail.com>
2021-06-11 00:00:37 -0500

    added save and load functions

--- lib/cards.ex
+++ lib/cards.ex
@@ -26,4 +26,18 @@ defmodule Cards do
     #returns hand as index 0, leftover deck as index 1
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist."
+    end
+  end
 end
